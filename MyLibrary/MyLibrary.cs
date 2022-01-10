07f8341d804a97d076319f64d8dcc8de5f909ae7
Alexey <nice.isakin@mail.ru>
2022-01-10 22:13:27 +0300

    Добавлена библиотека и класс, а также readme файл

---
+++ MyLibrary/MyLibrary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class MyLibrary
+    {
+        public static string NumCompare(int a, int b)
+        {
+            if (a > b)
+                return "больше";
+            if (a < b)
+                return "меньше";
+            else
+                return "равно";
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int GetRandom()
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(0, 10);
+            return value;
+        }
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyLib;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = MyLibrary.GetRandom();
+            int b = MyLibrary.GetRandom();
+            int c = MyLibrary.GetRandom();
+
+            Console.WriteLine($"{a}, {b}, {c}");
+
+            int value1 = MyLibrary.Add(a, b);
+            int value2 = MyLibrary.Add(a, c);
+
+            string result = MyLibrary.NumCompare(value1, value2);
+            Console.WriteLine($"value1 {result} value2");
+
+        }
+    }
+}
